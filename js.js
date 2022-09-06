3ab078c686ec1afadccb1824e01349a2c7c9dcbb
Abduqodir077 <urinovabduqodir07@gmail.com>
2022-09-06 21:54:49 +0500

    start

---
+++ js.js
@@ -0,0 +1,142 @@
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let btn5 = document.querySelector(".btn5");
+let btn6 = document.querySelector(".btn6");
+let btn7 = document.querySelector(".btn7");
+let btn8 = document.querySelector(".btn8");
+let btn9 = document.querySelector(".btn9");
+let btn10 = document.querySelector(".btn10");
+let btn11 = document.querySelector(".btn11");
+let btn12 = document.querySelector(".btn12");
+let btn13 = document.querySelector(".btn13");
+let btn14 = document.querySelector(".btn14");
+let btn15 = document.querySelector(".btn15");
+let btn16 = document.querySelector(".btn16");
+let btn17 = document.querySelector(".btn17");
+let btn18 = document.querySelector(".btn18");
+let btn19 = document.querySelector(".btn19");
+let btn20 = document.querySelector(".btn20");
+let btn21 = document.querySelector(".btn21");
+let btn22 = document.querySelector(".btn22");
+let btn23 = document.querySelector(".btn23");
+let btn24 = document.querySelector(".btn24");
+let btn25 = document.querySelector(".btn25");
+let btn26 = document.querySelector(".btn26");
+let btn27 = document.querySelector(".btn27");
+let btn28 = document.querySelector(".btn28");
+let btn29 = document.querySelector(".btn29");
+let btn30 = document.querySelector(".btn30");
+let btn31 = document.querySelector(".btn31");
+let btn32 = document.querySelector(".btn32");
+let btn33 = document.querySelector(".btn33")
+let btn34 = document.querySelector(".btn34");
+let btn35 = document.querySelector(".btn35");
+let btn36 = document.querySelector(".btn36");
+let btn37 = document.querySelector(".btn37");
+let btn38 = document.querySelector(".btn38");
+let btn39 = document.querySelector(".btn39");
+let btn40 = document.querySelector(".btn40");
+let btn41 = document.querySelector(".btn41");
+let btn42 = document.querySelector(".btn42");
+let btn43 = document.querySelector(".btn43");
+let btn44 = document.querySelector(".btn44");
+let btn45 = document.querySelector(".btn45");
+let btn46 = document.querySelector(".btn46");
+let btn47 = document.querySelector(".btn47");
+let btn48 = document.querySelector(".btn48");
+let btn49 = document.querySelector(".btn49");
+let btn50 = document.querySelector(".btn50");
+let btn51 = document.querySelector(".btn51");
+let btn52 = document.querySelector(".btn52");
+let btn53 = document.querySelector(".btn53");
+let btn54 = document.querySelector(".btn54");
+let btn55 = document.querySelector(".btn55");
+let btn56 = document.querySelector(".btn56");
+let btn57 = document.querySelector(".btn57");
+let btn58 = document.querySelector(".btn58");
+let btn59 = document.querySelector(".btn59");
+let btn60 = document.querySelector(".btn60");
+let btn61 = document.querySelector(".btn61");
+let btn62 = document.querySelector(".btn62");
+let btn63 = document.querySelector(".btn63");
+let btn64 = document.querySelector(".btn64");
+let btn65 = document.querySelector(".btn65");
+let btn66 = document.querySelector(".btn66");
+let btn67 = document.querySelector(".btn67");
+let btn68 = document.querySelector(".btn68");
+let btn69 = document.querySelector(".btn69");
+let btn70 = document.querySelector(".btn70");
+let btn71 = document.querySelector(".btn71");
+let btn72 = document.querySelector(".btn72");
+let btn73 = document.querySelector(".btn73");
+let btn74 = document.querySelector(".btn74");
+let btn75 = document.querySelector(".btn75");
+let btn76 = document.querySelector(".btn76");
+let btn77 = document.querySelector(".btn77");
+let btn78 = document.querySelector(".btn78");
+let btn79 = document.querySelector(".btn79");
+let btn80 = document.querySelector(".btn80");
+let btn81 = document.querySelector(".btn81");
+let btn82 = document.querySelector(".btn82");
+let btn83 = document.querySelector(".btn83");
+let btn84 = document.querySelector(".btn84");
+let btn85 = document.querySelector(".btn85");
+let btn86 = document.querySelector(".btn86");
+let btn87 = document.querySelector(".btn87");
+let btn88 = document.querySelector(".btn88");
+let btn89 = document.querySelector(".btn89");
+let btn90 = document.querySelector(".btn90");
+let btn91 = document.querySelector(".btn91");
+let btn92 = document.querySelector(".btn92");
+let btn93 = document.querySelector(".btn93");
+
+
+ btn1.addEventListener("click", function(){
+    btn1.style.background = "red";
+    btn1.style.color = "white";
+});
+
+ btn2.addEventListener("click", function(){
+    btn2.style.background = "red";
+    btn2.style.color = "white";
+});
+
+ btn3.addEventListener("click", function(){
+    btn3.style.background = "red";
+    btn3.style.color = "white";
+});
+
+ btn4.addEventListener("click", function(){
+    btn4.style.background = "red";
+    btn4.style.color = "white";
+});
+
+ btn5.addEventListener("click", function(){
+    btn5.style.background = "red";
+    btn5.style.color = "white";
+});
+
+ btn6.addEventListener("click", function(){
+    btn6.style.background = "red";
+    btn6.style.color = "white";
+});
+
+ btn7.addEventListener("click", function(){
+    btn7.style.background = "red";
+    btn7.style.color = "white";
+});
+
+ btn8.addEventListener("click", function(){
+    btn8.style.background = "red";
+    btn8.style.color = "white";
+});
+
+ btn9.addEventListener("click", function(){
+    btn9.style.background = "red";
+    btn9.style.color = "white";
+});
+
+
+
